93bf21b90a967dcbfdde69d102ef461baa68c0fb
Andre Veltens <158059006+swiftAndHandy@users.noreply.github.com>
2024-11-18 20:35:57 +0100

    Initial Commit

---
+++ WeSplit/WeSplitApp.swift
@@ -0,0 +1,17 @@
+//
+//  WeSplitApp.swift
+//  WeSplit
+//
+//  Created by Andre Veltens on 18.11.24.
+//
+
+import SwiftUI
+
+@main
+struct WeSplitApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
