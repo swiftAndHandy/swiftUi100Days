cdb3cec6760ba4c5e6cf99e07353fc07ac5c51de
Andre Veltens <158059006+swiftAndHandy@users.noreply.github.com>
2024-11-21 22:23:56 +0100

    Commit translation and app functionality

--- WeSplit/ContentView.swift
+++ WeSplit/ContentView.swift
@@ -8,14 +8,69 @@
 import SwiftUI
 
 struct ContentView: View {
+    @State private var totalPrice: Double = 0
+    @State private var numberOfPeople: Int = 2
+    @State private var tipPercentage: Int = 0
+    @FocusState private var totalIsFocused: Bool
+    
+    let tipPercentages: [Int] = [5, 10, 15, 20, 0]
+    
+    var totalPerPerson: Double {
+        let pplCount: Double = Double(numberOfPeople)
+        return ceil((totalAfterTip / pplCount) * 100) / 100
+    }
+    
+    var totalAfterTip: Double {
+        let tipSelection: Double = Double(tipPercentage)
+        let tipValue = totalPrice / 100 * tipSelection
+        return totalPrice + tipValue
+    }
+    
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+        NavigationStack {
+            Form {
+                Section("overview.splitInformation") {
+                    HStack {
+                        Text("overview.amountToSplit")
+                        TextField("overview.amountInput", value: $totalPrice, format: .currency(code: Locale.current.currency?.identifier ?? "EUR"))
+                            .keyboardType(.decimalPad)
+                            .multilineTextAlignment(.trailing)
+                            .focused($totalIsFocused)
+                    }
+                    HStack {
+                        Picker("overview.splitBetween", selection: $numberOfPeople) {
+                            ForEach(2...10, id: \.self) {
+                                Text("^[\($0) Person](inflect: true)")
+                            }
+                        }
+                    }
+                }
+                Section("overview.howMuchTip") {
+                    Picker("Tip percentage", selection: $tipPercentage) {
+                        ForEach(tipPercentages, id: \.self) {
+                            Text($0, format: .percent)
+                        }
+                    }
+                    .pickerStyle(.segmented)
+                }
+                Section("overview.totalAfterTip") {
+                    Text(totalAfterTip, format: .currency(code: Locale.current.currency?.identifier ?? "EUR"))
+                    
+                }
+                Section("overview.totalPerPersonSection") {
+                    Text(totalPerPerson, format: .currency(code: Locale.current.currency?.identifier ?? "EUR"))
+                }
+            }
+            .navigationTitle("WeSplit")
+            .navigationBarTitleDisplayMode(.inline)
+            .toolbar {
+                if totalIsFocused {
+                    Button("Done") {
+                        totalIsFocused = false
+                    }
+                }
+            }
         }
-        .padding()
     }
 }
 
