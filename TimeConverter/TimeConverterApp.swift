024f33030e08968e1184db07101bd9e72178eab2
Andre Veltens <158059006+swiftAndHandy@users.noreply.github.com>
2024-11-22 15:12:01 +0100

    Initial Commit

---
+++ TimeConverter/TimeConverterApp.swift
@@ -0,0 +1,17 @@
+//
+//  TimeConverterApp.swift
+//  TimeConverter
+//
+//  Created by Andre Veltens on 22.11.24.
+//
+
+import SwiftUI
+
+@main
+struct TimeConverterApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
