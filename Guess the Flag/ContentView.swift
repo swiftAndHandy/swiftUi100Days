0af280a34fd0149b05e8ccf1e225015ea989478b
Andre Veltens <158059006+swiftAndHandy@users.noreply.github.com>
2024-11-23 15:13:20 +0100

    Initial Commit

---
+++ Guess the Flag/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Guess the Flag
+//
+//  Created by Andre Veltens on 23.11.24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
